9d577d8827e470bdac77d75316786a23899973d4
amrelsayed <amrmostafa1995@gmail.com>
2022-03-09 17:57:15 +0200

    lifecycle-awareness when collecting flows from our HomeScreen.kt added

--- app/src/main/java/com/aelsayed/kaizen/presentation/screen/HomeScreen.kt
+++ app/src/main/java/com/aelsayed/kaizen/presentation/screen/HomeScreen.kt
@@ -6,18 +6,49 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.flowWithLifecycle
 import com.aelsayed.kaizen.R
 import com.aelsayed.kaizen.presentation.components.CategoryBar
 import com.aelsayed.kaizen.presentation.components.CustomTopAppBar
 import com.aelsayed.kaizen.presentation.components.MatchCard
+import com.aelsayed.kaizen.presentation.viewmodel.KaizenSportState
 import com.aelsayed.kaizen.presentation.viewmodel.KaizenSportViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+
+
+@Composable
+fun <T> rememberFlow(
+    flow: Flow<T>,
+    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current
+): Flow<T> {
+    return remember(key1 = flow, key2 = lifecycleOwner) { flow.flowWithLifecycle(lifecycleOwner.lifecycle, Lifecycle.State.STARTED) }
+}
+
+@Composable
+fun <T : R, R> Flow<T>.collectAsStateLifecycleAware(
+    initial: R,
+    context: CoroutineContext = EmptyCoroutineContext
+): State<R> {
+    val lifecycleAwareFlow = rememberFlow(flow = this)
+    return lifecycleAwareFlow.collectAsState(initial = initial, context = context)
+}
+
+@Suppress("StateFlowValueCalledInComposition")
+@Composable
+fun <T> StateFlow<T>.collectAsStateLifecycleAware(
+    context: CoroutineContext = EmptyCoroutineContext
+): State<T> = collectAsStateLifecycleAware(value, context)
 
 @Composable
 fun HomeScreen(viewModel: KaizenSportViewModel) {
@@ -27,7 +58,10 @@ fun HomeScreen(viewModel: KaizenSportViewModel) {
         CustomTopAppBar(title = stringResource(R.string.app_name))
     }) {
 
-        val state by viewModel.state.collectAsState()
+        //        val state by viewModel.state.collectAsState()
+
+        val state: KaizenSportState by viewModel.state.collectAsStateLifecycleAware()
+
 
         if (state.isLoading)
             Box(
@@ -120,5 +154,6 @@ fun HomeScreen(viewModel: KaizenSportViewModel) {
 
 
     }
+
 
 }
